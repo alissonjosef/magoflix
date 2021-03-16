c38a4a06aca302e019febf474b2eb3a62495b06c
alissonjosef <alissonjosef@gmail.com>
2021-03-16 11:31:54 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Menu from './components/Menu';
+
+
 
 
 function App() {
   return (
     <div>
-      <h1>Ola Alisson</h1>
-      quero ver se muda mesmo...
+      <Menu />
     </div>
   );
 }
