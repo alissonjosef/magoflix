4d48629cbba4f066bf3bbc5a351aa9ff5c36ba47
alissonjosef <alissonjosef@gmail.com>
2021-03-18 16:49:41 -0300

    link

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Menu from '../../components/Menu';
+/* import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer'; */
+
+
+
+function Home() {
+  return (
+    <div style={{ background: "#141414" }}>
+      <Menu />
+    </div>
+  );
+}
+
+export default Home;
