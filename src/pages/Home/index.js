6ec1b2cb521883b41de0300c22f8336da12159d9
alissonjosef <alissonjosef@gmail.com>
2021-04-16 13:47:12 -0300

    home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Menu from '../../components/Menu';
-/* import DadosIniciais from '../../data/dados_iniciais.json';
+import DadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
-import Carousel from '../../components/Carousel'; */
+import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
 
 
@@ -12,9 +12,38 @@ function Home() {
     <div style={{ background: "#141414" }}>
       <Menu />
 
-      <Footer />
+      <BannerMain 
+        videoTitle={DadosIniciais.categorias[0].videos[0].titulo}
+        url={DadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"o que e front-end? Trabalhando na area"}
+      />
 
-      
+      <Carousel 
+        ignoreFirstVideo
+        category={DadosIniciais.categorias[0]}
+      />
+
+      <Carousel 
+        category={DadosIniciais.categorias[1]}
+      />
+
+      <Carousel 
+        category={DadosIniciais.categorias[2]}
+      />
+
+      <Carousel 
+        category={DadosIniciais.categorias[3]}
+      />
+
+      <Carousel 
+        category={DadosIniciais.categorias[4]}
+      />
+
+      <Carousel 
+        category={DadosIniciais.categorias[5]}
+      />
+
+      <Footer />
 
     </div>
   );
