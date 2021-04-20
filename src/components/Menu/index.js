9868e24bfce7a8fa6d7afd467488ecf467c112cd
alissonjosef@gmail.com <alissonjosef@gmail.com>
2021-04-19 22:28:19 -0300

    cadastro

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,19 +3,20 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-//import ButtonLink from './components/ButtonLink';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
   return (
     <nav className="Menu">
       <Link to="/">
-        <img className="Logo" src={Logo} alt="Magoflix logo"/>
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
       </Link>
+
       <Button as={Link} className="ButtonLink" to="/cadastro/video">
-          Novo vídeo
+        Novo vídeo
       </Button>
     </nav>
   );
-
 }
+
 export default Menu;
